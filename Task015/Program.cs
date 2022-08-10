f1036b832df60d6c41c76b2b9d89a98031989902
Kira <starkial@lenta.ru>
2022-08-10 17:06:16 +0500

    Task 015 Определение выходного дня по номеру

---
+++ Task015/Program.cs
@@ -0,0 +1,8 @@
+﻿//принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру дня недели от 1 до 7 ");
+int number = Convert.ToInt32(Console.ReadLine( ));
+if (number > 0 && number < 6)
+    Console.Write("нет");
+if (number == 6 || number == 7)
+    Console.Write("да");
