7f825a9596dda6d504ee5a16041da3dd57302123
Kira <starkial@lenta.ru>
2022-08-10 15:54:29 +0500

    Task 010 добавлена программа решения

---
+++ Task010/Program.cs
@@ -0,0 +1,7 @@
+﻿// на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int digit1 = num / 10;
+int result = digit1 % 10;
+Console.Write($"Вторая цифра числа {result}");
