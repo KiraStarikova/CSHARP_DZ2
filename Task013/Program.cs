eec58fd73d45af0f84549a3e780f614b0a594dd4
Kira <starkial@lenta.ru>
2022-08-10 16:22:10 +0500

    Task 013 добавили программу вывод третьей цифры

---
+++ Task013/Program.cs
@@ -0,0 +1,10 @@
+﻿// выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+int num = new Random().Next(10, 100000);
+Console.WriteLine(num);
+int num1 = num / 100;
+int result = num1 % 10;
+if (result > 0)
+    Console.WriteLine($"Третье число {result}");
+else
+    Console.WriteLine("Третьей цифры нет");
